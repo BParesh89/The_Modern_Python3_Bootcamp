402d620b2b17bd123ea25090dff9269f70279176
Timothy Thatcher <thatcher.tim.tt@gmail.com>
2018-03-02 00:04:05 +1100

    a small change to rspComp.py

--- projects/rockpaperscissor/rspComp.py
+++ projects/rockpaperscissor/rspComp.py
@@ -3,14 +3,16 @@ from random import randint
 
 play_again = "y"
 
-while play_again == "y":
 
+while play_again == "y":
+	winning_score = int(input("What do you want the winning score to be? "))
+	
 	player_wins = 0
 	computer_wins = 0
 
 	print("...rock...\n...scissors...\n...paper...\n")
 
-	while player_wins < 2 and computer_wins < 2:
+	while player_wins < winning_score and computer_wins < winning_score:
 		if player_wins == 0 and computer_wins == 0:
 			pass
 		else:
@@ -28,8 +30,11 @@ while play_again == "y":
 		player = input("enter your choice: ").lower()
 
 		#checking to see if entered a valid choice
-		while player != "rock" and player != "paper" and player != "scissors":
+		while player != "rock" and player != "paper" and player != "scissors" and player != "quit":
 				player = input("please enter a valid choice: ")
+
+		if player == "quit":
+			break
 
 		#printing what the computer was randomly assigned
 		print(f"The computer was assigned: {computer}")
@@ -65,8 +70,10 @@ while play_again == "y":
 		else:
 			print("Not a legitimate game")
 
-	if player_wins == 2:
+	if player_wins > computer_wins:
 		play_again = input("You win the game!! \nDo you want to play again? (y/n) ")
+	elif player_wins == computer_wins:
+		play_again = input("It's a tie! \nDo you want to play again? (y/n) ")
 	else:
 		play_again = input("You lose the game. \nDo you want to play again? (y/n) ")
 
